eaff8434aa52576949ae8026cedae1101f44182a
Andrey <medveddevaa@icloud.com>
2020-10-17 18:07:44 +0300

    Tweet text limit

--- src/Components/CreateTweetForm/index.tsx
+++ src/Components/CreateTweetForm/index.tsx
@@ -12,18 +12,27 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 
 export default ()=>{
     const classes = styles();
+    const [text, setText] = useState<string>('');
+    const textLimitCircle: number = Math.floor((text.length / 260) * 100);
+    const symbolLeft: number = 260 - text.length;
+    const circleColor: string = text.length === 260 ? 'red' : '';
+
+    const handleTextArea = (e: React.FormEvent<HTMLTextAreaElement>) =>{
+        setText(e.currentTarget.value);
+    }
 
     return(
         <div className={classes.posttext}>
             <div className={classes.postEnter}>
                 <Avatar className={classes.postavatar} alt="Remy Sharp"  />
                 <TextareaAutosize 
+                    onChange={handleTextArea}
                     className={classes.textarea}
                     aria-label="Textarea post"
                     placeholder='Что нового ?'
                     rowsMin={1}
                     maxLength={260}
-                    rowsMax={5}
+                    value={text}
                 />
             </div>
             <div className={classes.createPostNav}>
@@ -42,11 +51,14 @@ export default ()=>{
                     <EmojiEmotionsOutlinedIcon />
             </IconButton>
             <div style={{marginLeft: 'auto', position: 'relative'}}>
-                <span style={{marginRight: 20}}>260</span>
-                <CircularProgress variant='static' value={100} style={{position: 'absolute',zIndex: 2,top: '40%',
-                    left: '22%',transform: 'translateY(-50%)',color: 'gray', width: '20px', height: 20}} />
-                <CircularProgress thickness={3.6} variant='static' value={50} style={{position: 'absolute',zIndex: 3,top: '40%',
-                    left: '22%',transform: 'translateY(-50%) rotate(-90deg)', width: '20px', height: 20}} />
+                { text && <>
+                    <span style={{marginRight: 20, width: 24, display: 'inline-block'}}>{symbolLeft}</span>
+                    <CircularProgress variant='static' value={100} style={{position: 'absolute',zIndex: 2,top: '40%',
+                        left: '22%',transform: 'translateY(-50%)',color: 'gray', width: '20px', height: 20}} />
+                    <CircularProgress thickness={3.6} variant='static' value={textLimitCircle} style={{position: 'absolute',zIndex: 3,top: '40%',
+                        left: '22%',transform: 'translateY(-50%) rotate(-90deg)', width: '20px', height: 20, color: circleColor  }} />
+                    </>
+                }
                 <Button style={{marginLeft: '10px'}} variant="contained" color="primary">Tweet</Button>
             </div>
         </div>
